c045012a1eeb5e1dc8cfd8c3c844394882c352c4
Manh Hung <manhhung120503@gmail.com>
2022-09-08 14:00:39 +0700

    init commit

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class loginController extends Controller
+{
+    // public function login(){
+    //     $login = Login::all();
+
+    //     return view('/login');
+    // }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,80 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>LOGIN</title>
+    <script src="js/jquery.min.js"></script>
+    <!-- Custom Theme files -->
+    <link href="css/style-login.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- for-mobile-apps -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Classy Login form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+    />
+    <!-- //for-mobile-apps -->
+    <!--Google Fonts-->
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+    <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <!--header start here-->
+    <div class="header">
+        <div class="header-main">
+            <h1>LOGIN USER</h1>
+            <div class="header-bottom">
+                <div class="header-right w3agile">
+
+                    <div class="header-left-bottom agileinfo">
+
+                        <form action="#" method="post">
+                            <input type="text" value="User name" name="name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}" />
+                            <input type="password" value="Password" name="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}" />
+                            <div class="remember">
+                                <span class="checkbox1">
+							   <label class="checkbox"><input type="checkbox" name="" checked=""><i> </i>Remember me</label>
+						 </span>
+                                <div class="forgot">
+                                    <h6><a href="#">Forgot Password</a> | <a href="#">Sign Up.</a></h6>
+                                </div>
+                                <div class="clear"> </div>
+                            </div>                        
+                                <a class="login" href="#">LOGIN</a>
+                        </form>
+                        <div class="header-left-top">
+                            <div class="sign-up">
+                                <h2>or</h2>
+                            </div>
+
+                        </div>
+                        <!-- <div class="header-social wthree">
+							<a href="#" class="face"><h5>Facebook</h5></a>
+							<a href="#" class="twitt"><h5>Twitter</h5></a>
+					    </div> -->
+                        <ul class="social-agileinfo wthree2">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--header end here-->
+    <div class="copyright">
+        <p>For any support please contact via: <a href="https://support.apple.com/vi-vn/guide/watch/apd7eafc2e91/watchos" target="_blank"> Apple Watches </a>| <a href="https://www.samsung.com/vn/support/contact/" target="_blank">Galaxy Watches </a>| <a href="https://www.mi.com/vn/support/repair/">Xiaomi Watches</a>
+        </p>
+    </div>
+    <!--footer end here -->
+</body>
+
+</html>
